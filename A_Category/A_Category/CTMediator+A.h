fc295c3aa9ecd2800d2e1da1dff7406a9d580e63
sunguan <sun_guan@suixingpay.com>
2018-11-21 16:06:24 +0800

    1

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,19 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by zwy on 2018/11/21.
+//  Copyright © 2018年 suixingpay. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (A)
+
+- (UIViewController *)A_aViewController;
+
+@end
+
+NS_ASSUME_NONNULL_END
